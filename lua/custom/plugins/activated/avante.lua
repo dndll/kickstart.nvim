39903bb0ea8452a9fdbf7259874c2bcef027ea84
Donovan Dall <d@nuff.tech>
2025-01-16 08:47:04 +0000

    live grep is back

--- lua/custom/plugins/activated/avante.lua
+++ lua/custom/plugins/activated/avante.lua
@@ -1,107 +1,107 @@
 return {
-    "yetone/avante.nvim",
-    config = function()
-      require("avante").setup({
-        provider = "deepseek",
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          model = "claude-3-5-sonnet-20240620",
-          temperature = 0,
-          max_tokens = 4096,
+  "yetone/avante.nvim",
+  config = function()
+    require("avante").setup({
+      provider = "deepseek",
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-5-sonnet-20240620",
+        temperature = 0,
+        max_tokens = 4096,
+      },
+      vendors = {
+        deepseek = {
+          endpoint = "https://api.deepseek.com/chat/completions",
+          model = "deepseek-chat",
+          api_key_name = "DEEPSEEK_API_KEY",
+          max_tokens = 16384,
+          parse_curl_args = function(opts, code_opts)
+            return {
+              url = opts.endpoint,
+              headers = {
+                ["Accept"] = "application/json",
+                ["Content-Type"] = "application/json",
+                ["Authorization"] = "Bearer " .. os.getenv(opts.api_key_name),
+              },
+              body = {
+                model = opts.model,
+                messages = require("avante.providers").copilot.parse_message(code_opts),
+                temperature = 0,
+                max_tokens = 16384,
+                stream = true,
+              },
+            }
+          end,
+          parse_response_data = function(data_stream, event_state, opts)
+            require("avante.providers").copilot.parse_response(data_stream, event_state, opts)
+          end,
         },
-        vendors = {
-          deepseek = {
-            endpoint = "https://api.deepseek.com/beta/chat/completions",
-            model = "deepseek-coder",
-            api_key_name = "DEEPSEEK_API_KEY",
-            max_tokens = 8192,
-            parse_curl_args = function(opts, code_opts)
-              return {
-                url = opts.endpoint,
-                headers = {
-                  ["Accept"] = "application/json",
-                  ["Content-Type"] = "application/json",
-                  ["Authorization"] = "Bearer " .. os.getenv(opts.api_key_name),
-                },
-                body = {
-                  model = opts.model,
-                  messages = require("avante.providers").copilot.parse_message(code_opts),
-                  temperature = 0,
-                  max_tokens = 8192,
-                  stream = true,
-                },
-              }
-            end,
-            parse_response_data = function(data_stream, event_state, opts)
-              require("avante.providers").copilot.parse_response(data_stream, event_state, opts)
-            end,
-          },
-        },
-        behaviour = {
-          auto_suggestions = true,
-          auto_set_highlight_group = true,
-          auto_set_keymaps = true,
-          auto_apply_diff_after_generation = false,
-          support_paste_from_clipboard = true,
-        },
-        mappings = {
-          diff = {
-            ours = "qo",
-            theirs = "qt",
-            all_theirs = "qa",
-            both = "qb",
-            cursor = "qc",
-            next = "]x",
-            prev = "[x",
-          },
-          suggestion = {
-            accept = "<D-l>",
-            next = "<D-j>",
-            prev = "<D-k>",
-            dismiss = "<D-h>",
-          },
-          jump = {
-            next = "]]",
-            prev = "[[",
-          },
-          submit = {
-            normal = "<CR>",
-            insert = "<C-s>",
-          },
-        },
-        hints = { enabled = true },
-        windows = {
-          position = "right",
-          wrap = true,
-          width = 30,
-          sidebar_header = {
-            align = "center",
-            rounded = true,
-          },
-        },
-        highlights = {
-          diff = {
-            current = "DiffText",
-            incoming = "DiffAdd",
-          },
-        },
+      },
+      behaviour = {
+        auto_suggestions = true,
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = true,
+      },
+      mappings = {
         diff = {
-          autojump = true,
-          list_opener = "copen",
+          ours = "qo",
+          theirs = "qt",
+          all_theirs = "qa",
+          both = "qb",
+          cursor = "qc",
+          next = "]x",
+          prev = "[x",
         },
-      })
-      vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-        callback = function()
-          require("avante_lib").load()
-        end,
-      })
-    end,
-    dependencies = { "nvim-lua/plenary.nvim" },
-    event = "VeryLazy",
-      lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make BUILD_FROM_SOURCE=true",
+        suggestion = {
+          accept = "<D-l>",
+          next = "<D-j>",
+          prev = "<D-k>",
+          dismiss = "<D-h>",
+        },
+        jump = {
+          next = "]]",
+          prev = "[[",
+        },
+        submit = {
+          normal = "<CR>",
+          insert = "<C-s>",
+        },
+      },
+      hints = { enabled = true },
+      windows = {
+        position = "right",
+        wrap = true,
+        width = 30,
+        sidebar_header = {
+          align = "center",
+          rounded = true,
+        },
+      },
+      highlights = {
+        diff = {
+          current = "DiffText",
+          incoming = "DiffAdd",
+        },
+      },
+      diff = {
+        autojump = true,
+        list_opener = "copen",
+      },
+    })
+    vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+      callback = function()
+        require("avante_lib").load()
+      end,
+    })
+  end,
+  dependencies = { "nvim-lua/plenary.nvim" },
+  event = "VeryLazy",
+  lazy = false,
+  version = "*", -- set this if you want to always pull the latest change
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
@@ -109,7 +109,7 @@ return {
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    -- "zbirenbaum/copilot.lua",      -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
@@ -122,8 +122,8 @@ return {
           drag_and_drop = {
             insert_mode = true,
           },
-          -- required for Windows users
-          use_absolute_path = true,
+          -- -- required for Windows users
+          -- use_absolute_path = true,
         },
       },
     },
@@ -234,3 +234,4 @@ return {
 --     list_opener = "copen",
 --   },
 -- })
+
